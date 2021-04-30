aea22d2144fd5b5c975cdfcea2603cfded9144f6
Zebio <mateuseusebio@gmail.com>
2021-04-29 21:29:03 -0300

    Organização do código e Comentários

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+/*-----------------------Bibliotecas de Projeto---------------*/
 #include <esp_http_server.h>
 #include <stdio.h>
 #include <esp_wifi.h>
@@ -10,15 +11,17 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 
-#include <tcpip_adapter.h>
 
-/* The event group allows multiple bits for each event, but we only care about two events:
- * - we are connected to the AP with an IP
- * - we failed to connect after the maximum amount of retries */
+/*------------------------Definições de Projeto-----------------------*/
+/* O event Group permite muitos eventos por grupo mas nos só nos importamos com 2 eventos:
+ * - Conseguimos conexão com o AP com um IP
+ * - Falhamos na conexão apos o número máximo de tentativas*/
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
 
 
+/*Aqui definimos o SSID, a Senha e o número máximo de tentativas que vamos 
+tentar ao se conectar à rede Wireless*/
 #define EXAMPLE_ESP_WIFI_SSID      "tira_o_zoio"
 #define EXAMPLE_ESP_WIFI_PASS      "jabuticaba"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  10
@@ -28,17 +31,19 @@
 #define led2 33
 
 
-/*---------------------Variáveis GLobais    ----------------------*/
+/*---------------------Variáveis GLobais----------------------*/
+//armazena os status dos LEDs, True=ligado, False=Desligado
 bool led1_status = false;
 bool led2_status = false;
 
-
+//Handle dos Eventos Wireless
 static EventGroupHandle_t s_wifi_event_group;
 
-tcpip_adapter_ip_info_t ipInfo; 
-char str[256];
+//Declaração do server http
+static httpd_handle_t server =NULL;
 
-/*---------------------Variáveis para Armazenar código HTML ----------------------*/
+
+/*---------------------Constantes Cstring para Armazenar os códigos HTML ----------------------*/
 const char *index_html=       "<!DOCTYPE html><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"icon\" href=\"data:,\"></head><link rel=\"icon\" href=\"data:,\"><title>Projeto05 - Automacao com ESP32</title><style> html{color: #ffffff;font-family: Verdana;text-align: center;background-color: #272727fd}.botao_on{color: #ffffff;padding: 15px 25px;font-size: 30px;margin: 2px;font-family: Tahoma;background-color: #313891a6;}.botao_off{color: #ffffff;padding: 15px 25px;font-size: 30px;margin: 2px;font-family: Tahoma;background-color: #7c7c7ca6;}</style><body><h2>Controle de Saidas</h2><p>";
 const char *led1_ligado=      "<a href=\"/led1\"><button class=\"botao_on\" > LED 1   </button></a></p>";
 const char *led1_desligado=   "<a href=\"/led1\"><button class=\"botao_off\" > LED 1   </button></a></p>";
@@ -52,8 +57,87 @@ static const char *TAG = "ESP";
 static int s_retry_num = 0;
 
 
+/*-------------Declaração das Funções---------------------*/
+/*Imprimimos a Webpage de acordo com o estado dos LEDS, Usamos os Ifs, para de acordo com o
+estado dos Leds, enviar o código HTML referente ao LED lidago ou Desligado*/
+void print_webpage(httpd_req_t *req);
+
+//handler do Get da Página Principal
+static esp_err_t main_page_get_handler(httpd_req_t *req);
+
+//handler do Get do Led1. Altera o Valor do LED e imprime a página novamente
+static esp_err_t led1_get_handler(httpd_req_t *req);
+
+//handler do Get do Led2. Altera o Valor do LED e imprime a página novamente
+static esp_err_t led2_get_handler(httpd_req_t *req);
+
+//Cria o Server, Faz as configurações Padrão e Inicia os URI Handlers para os GETs
+static httpd_handle_t start_webserver(void);
+
+//Lida com os Eventos da rede Wireless(reconexão, IP recebido)
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data);
+
+//Configura a rede wireless
+void wifi_init_sta(void);
+
+
+
+/*------------------Declaração dos GETs do http-------------------*/
+//a declaração do GET da página Principal
+static const httpd_uri_t main_page = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = main_page_get_handler,
+    .user_ctx  = NULL
+};
+
+//a declaração do GET do Led1
+static const httpd_uri_t led1_get = {
+    .uri       = "/led1",
+    .method    = HTTP_GET,
+    .handler   = led1_get_handler,
+    /* Let's pass response string in user
+     * context to demonstrate it's usage */
+    .user_ctx  = NULL
+};
+
+//a declaração do GET do Led2
+static const httpd_uri_t led2_get = {
+    .uri       = "/led2",
+    .method    = HTTP_GET,
+    .handler   = led2_get_handler,
+    .user_ctx  = NULL
+};
+
+/*---------------------Função Principal----------------------------*/
+void app_main()
+{
+    //Inicializa a memória nvs pois é necessária para o funcionamento do Wireless
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    /*Nos conectamos ao Wireless*/
+    wifi_init_sta();
+
+
+    /* Inicia o Server pela primeira vez */
+    server = start_webserver();
+}
+
+/*----------------------Funções Auxiliares----------------------*/
+
+
+/*Imprimimos a Webpage de acordo com o estado dos LEDS, Usamos os Ifs, para de acordo com o
+estado dos Leds, enviar o código HTML referente ao LED lidago ou Desligado*/
 void print_webpage(httpd_req_t *req)
 {
+    //vamos concatenando os "pedaços" do html no buffer de acordo com os valores das variáveis
     char buffer[760] =""; 
     strcat(buffer, index_html);
 
@@ -70,7 +154,6 @@ void print_webpage(httpd_req_t *req)
     strcat(buffer, html_final);  
 
     httpd_resp_send(req, buffer, strlen(buffer));
-
 }
 
 
@@ -83,13 +166,6 @@ static esp_err_t main_page_get_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
-//a declaração da página Principal
-static const httpd_uri_t main_page = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = main_page_get_handler,
-    .user_ctx  = NULL
-};
 
 //handler do Get do Led1. Altera o Valor do LED e imprime a página novamente
 static esp_err_t led1_get_handler(httpd_req_t *req)
@@ -102,15 +178,6 @@ static esp_err_t led1_get_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
-//a declaração da página do Led1
-static const httpd_uri_t led1_get = {
-    .uri       = "/led1",
-    .method    = HTTP_GET,
-    .handler   = led1_get_handler,
-    /* Let's pass response string in user
-     * context to demonstrate it's usage */
-    .user_ctx  = NULL
-};
 
 
 //handler do Get do Led1. Altera o Valor do LED e imprime a página novamente
@@ -124,39 +191,30 @@ static esp_err_t led2_get_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
-
-//a declaração da página do Led2
-static const httpd_uri_t led2_get = {
-    .uri       = "/led2",
-    .method    = HTTP_GET,
-    .handler   = led2_get_handler,
-    .user_ctx  = NULL
-};
-
+//Cria o Server, Faz as configurações Padrão e Inicia os URI Handlers
+//para os GETs
 static httpd_handle_t start_webserver(void)
 {
     httpd_handle_t server = NULL;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     config.lru_purge_enable = true;
 
-    // Start the httpd server
-    printf("Starting server on port: '%d'", config.server_port);
+    // Inicia o server httpd
+    printf("Iniciando o Server na Porta: '%d'\n", config.server_port);
     if (httpd_start(&server, &config) == ESP_OK) {
         // Set URI handlers
-        printf("Registering URI handlers");
+        printf("Registrando URI handlers\n");
         httpd_register_uri_handler(server, &main_page);
         httpd_register_uri_handler(server, &led1_get);
         httpd_register_uri_handler(server, &led2_get);
         return server;
     }
 
-    printf("Error starting server!");
+    printf("Erro ao iniciar Server\n");
     return NULL;
 }
 
-
-
-
+//Lida com os Eventos da rede Wireless
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
 {
@@ -249,23 +307,4 @@ void wifi_init_sta(void)
     ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
     ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
     vEventGroupDelete(s_wifi_event_group);
-}
-
-
-void app_main()
-{
-    //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
-    wifi_init_sta();
-
-    static httpd_handle_t server =NULL;
-    /* Start the server for the first time */
-    server = start_webserver();
 }
